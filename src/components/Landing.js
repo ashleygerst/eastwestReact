82f82add902e345ca98d292bc1db7b5ee789aa54
ashleygerst <gerst.ashley@gmail.com>
2020-10-22 10:06:21 -0600

    updated navigation

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,13 +1,12 @@
-import { Route, BrowserRouter as Router, Switch, Redirect } from "react-router-dom";
+import { Route, BrowserRouter as Router, Switch, Redirect } from 'react-router-dom';
 import React, { Component } from 'react';
-import { Button } from 'reactstrap';
 import Header from './Header';
 import Footer from './Footer';
 import Navigation from './Nav';
 import About from '../pages/About';
-// import Contact from '../pages/Contact';
+import Contact from '../pages/Contact';
 import Home from '../pages/Home';
-// import Portfolio from '../pages/Portfolio';
+import Portfolio from '../pages/Portfolio';
 
 class Main extends Component {
   render() {
@@ -16,14 +15,20 @@ class Main extends Component {
       <Router>
         <Navigation />
         <Header />
-          <Route path='/' component={Home}>
-            <Home />
-          </Route>
-          <Route path='/about' render={() => <About/> }>
-            <About />
-          </Route>
-                {/* <Route path='/portfolio' component={Portfolio} />
-                <Route path='/contact' component={Contact} /> */}
+          <Switch>
+            <Route path='/about'>
+              <About />
+            </Route>
+            <Route path='/portfolio'>
+              <Portfolio />
+            </Route>
+            <Route path='/contact'>
+              <Contact />
+            </Route>
+            <Route path='/'>
+              <Home />
+            </Route>
+          </Switch>
         <Footer />
         </Router>
     );
