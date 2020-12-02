03bdc5b7552437767aa9c2b5fec68d6954706fa7
ashleygerst <gerst.ashley@gmail.com>
2020-12-01 20:41:05 -0700

    updated jumbotron css

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,15 +7,15 @@ class Header extends Component {
     console.log(this.props, 'image')
     return (
       <div>
-        <Jumbotron fluid className={'jumbotron'}>
-          <Container className='jumboContent' fluid>
-          <Media src={this.props.image} className={'jumbotronImage'} fluid />
-            <div className={'jumboText'}>
-              <h1 className='jumboTitle'>East West Manufacturing</h1>
-              <p className='jumboHeader'>Welcome to East West Manufacturing! East West Manufacturing & Supply Co., Inc. is a commercial 
-                        <br /> Mechanical Contractor specializing in vintage and contemporary HVAC systems.</p>
-                        <Button color='warning' href='/about'>About Us</Button>
-            </div>
+        <Jumbotron className='jumbotron'>
+          <Container className='jumboContent'>
+              <Media fluid src={this.props.image} className={'jumbotronImage'} />
+              <div className={'jumboText'}>
+                <h1 className='jumboTitle'>East West Manufacturing</h1>
+                <p className='jumboHeader'>Welcome to East West Manufacturing! East West Manufacturing & Supply Co., Inc. is a commercial 
+                <br /> Mechanical Contractor specializing in vintage and contemporary HVAC systems.</p>
+                <Button color='warning' href='/about'>About Us</Button>
+              </div>
           </Container>
         </Jumbotron>
       </div>
